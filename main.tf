59a7812a60538dec14cdfb62fd43943866580e25
Aditya Gurjar <136295362+aditytm@users.noreply.github.com>
2023-12-04 17:36:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
     region = "us-east-1"
 }
 # plan - execute
-resource "aws_s3_bucket" "test-s3-bucket"{
- bucket= "rari"
+resource "aws_s3_bucket" "test-s3-bucket2"{
+ bucket= "eaedrti"
 }
